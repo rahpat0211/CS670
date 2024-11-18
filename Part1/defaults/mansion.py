4f7ff5925bb045adde0a8d29ef45d0aecaf14469
rahpat0211 <80171784+rahpat0211@users.noreply.github.com>
2024-11-18 18:45:48 -0500

    Add in block comment summarizing the process for the rooms in the mansion.
    Options to return back the following:
     - String representation of the rooms in the mansion
     - List of all rooms in the mansion

--- Part1/defaults/mansion.py
+++ Part1/defaults/mansion.py
@@ -1,5 +1,8 @@
 class Mansion:
 	def __init__(self):
+		"""
+		Initialize a list of rooms available throughout the mansion
+		"""
 		self.rooms = [
 			"Kitchen",
 			"Ballroom",
@@ -12,6 +15,8 @@ class Mansion:
 			"Billiard Room"
 		]
 	
+	# Return a string representation of the rooms in the mansion
 	def __str__(self) -> str: return "Mansion Layout: " + ", ".join(self.rooms)
 	
+	# Return a list of all the rooms in the mansion
 	def get_rooms(self) -> list: return self.rooms
