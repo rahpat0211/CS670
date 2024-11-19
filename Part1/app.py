d019f865bd11c3ff630aec7f9ea110b740c22e44
rahpat0211 <80171784+rahpat0211@users.noreply.github.com>
2024-11-18 19:04:10 -0500

    Adding in comments to provide information regarding the overall process and different class methods.

--- Part1/app.py
+++ Part1/app.py
@@ -1,3 +1,5 @@
+# Import the required libraries after following steps 1 and 2 from README.md
+
 from wsgiref.validate import validator
 
 from defaults.weapons import *
@@ -10,43 +12,60 @@ import os
 import platform
 import logging
 
+# ----------------------------------------------------------------------------------------------------------------------
+
 class ClueGame:
 	def __init__(self):
-		self.solution = gather_solution()
+		self.solution = gather_solution() # Generate a dictionary format of the solution
+		
+		# Set the following to an empty string for the initial selection for the character
 		self.selected_character = ''
 		self.selected_room = ''
 		self.selected_weapon = ''
 	
 	def summary_generation(self):
-		# Ensure the Layout directory exists
+		"""
+		Generation of a xlsx file that consists of the overall Game Layout. Including a generated solution.
+		Don't need to run this if you don't want to see the solution.
+		Comment out clue.summary_generation() in clue_game_handler() if you wish to skip this.
+		"""
+		
+		# Directory Validation to store the file
 		layout_directory = "Layout"
 		if not os.path.exists(layout_directory):
 			os.makedirs(layout_directory)
 			logging.info(f"Created directory: {layout_directory}")
 		
+		# Create an empty dataframe for each sheet by creating the columns
 		character_sheet = pd.DataFrame(columns=["Character Name", "Starting Position"])
 		mansion_sheet = pd.DataFrame(columns=["Rooms"])
 		weapons_sheet = pd.DataFrame(columns=["Weapons"])
 		solution_sheet = pd.DataFrame(columns=["Character", "Weapon", "Room"])
 		
+		
+		# Population of the Character Sheet
 		for character in characters:
 			character_sheet = character_sheet._append({
 				"Character Name":    character.name,
 				"Starting Position": character.position},
 				ignore_index=True)
 		
+		# Population of the Mansion Sheet
 		for room in Mansion().get_rooms():
 			mansion_sheet = mansion_sheet._append({"Rooms": room}, ignore_index=True)
 		
+		# Population of the Weapons Sheet
 		for tools in list(weapons.values()):
 			weapons_sheet = weapons_sheet._append({"Weapons": tools}, ignore_index=True)
 		
+		# Population of the Solution Sheet
 		solution_sheet = solution_sheet._append({
 			"Character": self.solution.get('character'),
 			"Weapon":    self.solution.get('weapon'),
 			"Room":      self.solution.get('room')},
 			ignore_index=True)
 		
+		# Initialize the file name by joining the file name with the parent directory
 		file_name = os.path.join(layout_directory, "Game_Set_Up.xlsx")
 		try:
 			with pd.ExcelWriter(file_name, engine='openpyxl') as writer:
@@ -69,11 +88,19 @@ class ClueGame:
 			os.system(f"xdg-open {file_name}")
 	
 	def character_selection(self):
+		"""
+		Selection of a character from a list.
+		Validation to ensure the character input is in the list
+		Retry mechanism for any typos
+		"""
+		
+		# Dictionary of available characters and their starting position
 		info = {character.name: character.position for character in characters}
 		
 		while True:  # Loop until a valid character is selected
 			selected_character = input(f"Please choose a character from this list: {list(info.keys())}: ")
 			
+			# Validator to ensure character selection is valid
 			if selected_character in list(info.keys()):
 				selected_room = info.get(selected_character)
 				print(f"You have selected: {selected_character}")
@@ -83,20 +110,34 @@ class ClueGame:
 				self.selected_room = selected_room
 				return
 			else:
+				# Retrial of character selection if choice is not valid
 				print(f"Sorry, '{selected_character}' is not a valid character. Please try again.")
 	
 	def character_movements(self):
+		"""
+		Process for the user to choice between moving to rooms or making a suggestion
+		Validation for each prompt input
+		Retry mechanism for any typos
+		"""
+		
 		def move_room():
+			# Moving to a different room
 			while True:
 				selected_room = input(f"Select a room you would like to move to: {Mansion().get_rooms()}: ")
+				
+				# Validation check to ensure room selected is within the list
 				if selected_room in Mansion().get_rooms():
 					print(f"You have selected: {selected_room}")
 					self.selected_room = selected_room
 					return
 				else:
+					# Retrial of room selection if choice is not valid
 					print(f"Sorry, '{selected_room}' is not a valid room. Please try again.")
 		
 		def make_suggestion():
+			# Making a suggestion based on user inputs of a character, weapon and room
+			
+			# Information to valid on
 			info = {character.name: character.position for character in characters}
 			weapon_list = list(weapons.values())
 			rooms = Mansion().get_rooms()
@@ -119,11 +160,13 @@ class ClueGame:
 					return False
 			
 			def validator(question, actual):
+				# Ensure the input matches that of the validated information
 				while True:
 					resp = input(question)
 					if resp in actual:
 						return resp
 					else:
+						# Retrial of input if the choice is not valid
 						print(f"Sorry, your input does not match the records in the system. Please try again.")
 			
 			attempt = 1
@@ -177,6 +220,13 @@ class ClueGame:
 	
 				
 def clue_game_handler():
+	"""
+	Main handler of the Clue Game.
+	Initialization of the main class
+	Calling class methods
+	
+	Please comment line 231 if you don't want to download the Game Layout nor Solution
+	"""
 	clue = ClueGame()
 	clue.summary_generation()
 	clue.character_selection()
